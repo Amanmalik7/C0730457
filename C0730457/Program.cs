fed9f499995dcfac6e966ce4a4bc729a59ed672d
visualstudio <visualstudio@122-PC13>
2019-02-06 11:36:38 -0500

    11 30 commit

--- C0730457/Program.cs
+++ C0730457/Program.cs
@@ -8,30 +8,17 @@ namespace C0730457
 {
     class Program
     {
-        static int i = 0;
-        static void Main(string[] args)
         
-            {
-                MethodA();
-            }
+        static void Main(string[] args)
+        {
 
-             static void MethodA()
+        }
+        class dog
+        {
 
-            {
-                while (Peanut())
-                {
-                    Console.WriteLine(i);
-                    if (i > 10)
-                    {
-                        return;
-                    }
-                }
-            }
-             static bool Peanut()
-            {
-                i++;
-                return true;
-            }
+        }
+            
+               
         }
     }
 
