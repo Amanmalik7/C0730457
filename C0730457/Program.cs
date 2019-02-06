d8233e0eb44edeff07599faaeed839e599cac6f4
visualstudio <visualstudio@122-PC13>
2019-02-06 10:28:23 -0500

    com

--- C0730457/Program.cs
+++ C0730457/Program.cs
@@ -8,8 +8,31 @@ namespace C0730457
 {
     class Program
     {
+        static int i = 0;
         static void Main(string[] args)
-        {
+        
+            {
+                MethodA();
+            }
+
+             static void MethodA()
+
+            {
+                while (Peanut())
+                {
+                    Console.WriteLine(i);
+                    if (i > 10)
+                    {
+                        return;
+                    }
+                }
+            }
+             static bool Peanut()
+            {
+                i++;
+                return true;
+            }
         }
     }
-}
+
+
